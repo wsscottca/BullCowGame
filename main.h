1eb1eb614efe9ab5545b31dd65e9cb42ab08ed27
Will Scott <WSScottCA@gmail.com>
2017-02-19 17:50:00 -0800

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#pragma once
+
+int main();
+
+// Initialize the functions
+void PrintASCII();
+void PrintIntro();
+int GetValidDifficulty();
+std::string GetValidWord();
+void Reset();
+void PlayGame();
+std::string GetValidGuess();
+bool QueryPlayAgain();
+void PrintSummary();
